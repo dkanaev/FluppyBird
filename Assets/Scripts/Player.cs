05fe29dcc5eb178652b666f9bd074c52bb1ffc74
dkanaev <KanayevDmitriy325@gmail.com>
2023-01-07 15:16:16 +0400

    animations and menu
    a

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
-
     public float junmpForce;
+    public BirdAnimation birdAnimation;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
+            rb.velocity =  new Vector2();
             rb.AddForce(transform.up * junmpForce, ForceMode2D.Impulse);
+            birdAnimation.StartRotation();
         }
+        birdAnimation.ApplyRotation(rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -23,7 +26,10 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("PipePart"))
         {
             GameManager.instance.Lose();
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if(other.gameObject.CompareTag("Floor"))
+        {
+            GameManager.instance.Lose();
         }
     }
 }
